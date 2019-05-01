74e3feb649d60362c7f33b14426e133ae30e3ce6
Salomon Häseli <salomon.haeseli@students.fhnw.ch>
2019-05-01 12:02:21 +0200

    worked on generatePassword (do not use Apache common-math3 library)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> Z = generateZ();
-        generatePassword(lengthOfPassword);
+        //generatePassword(lengthOfPassword);
 
     }
 
